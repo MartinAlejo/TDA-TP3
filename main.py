76070a96aec77705166e1236bf7a47920ac8b9e8
RiedelNicolas <nariedel99@gmail.com>
2024-11-16 14:25:00 -0300

    algorithm working for ships of lenght 1

---
+++ main.py
@@ -0,0 +1,75 @@
+def print_grid(grid):
+    print("----------------------")
+    for row in grid:
+        for cell in row:
+            if cell == None:
+                print('-', end=' ')
+            else:
+                print(cell, end=' ')
+        print("") 
+
+def parse_input (path):
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        #Remove lines that start with #, and remove the newline character
+        lines = [line for line in lines if not line.startswith('#')]
+        lines = [line.rstrip() for line in lines]
+        rows, cols , ships = [],[],[]
+        aux_counter = 0
+        for x in lines:
+            if x == '':
+                aux_counter += 1
+                continue
+            if aux_counter == 0:
+                rows.append(int(x))
+            if aux_counter == 1:
+                cols.append(int(x))
+            if aux_counter == 2:
+                ships.append(int(x))
+        return rows, cols, ships
+    
+#A position is valid if it is not occupied by a ship, and it is not adjacent to a ship.
+# positions without ships are marked with None    
+def verify_position(grid, row, col):
+    if grid[row][col] != None:
+        return False
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if row + i >= 0 and row + i < len(grid) and col + j >= 0 and col + j < len(grid[0]):
+                if grid[row + i][col + j] != None:
+                    return False
+    return True
+
+#Return a list of list, showing the placement of the ships
+def ship_placement(rows, cols, ships):
+    grid = [[None] * len(rows) for _ in range(len(cols))]
+    gained_ammount = 0
+    total_amount = sum(rows) + sum(cols)
+    gained_ammount = ship_placemente_aux(rows, cols, ships, grid, 0)
+    print("Gained ammount: ", gained_ammount)
+    print("Total ammount: ", total_amount)
+    print_grid(grid)
+
+def ship_placemente_aux(rows, cols, ships, grid, current_ship):
+    if current_ship == len(ships):
+        return 0
+    
+    for i in range(len (rows)):
+        for j in range(len (cols)):
+            if (rows[i] > 0 and cols[j] > 0) and verify_position(grid, i, j):
+                grid[i][j] = current_ship
+                rows[i] -= 1
+                cols[j] -= 1
+                return 2 + ship_placemente_aux(rows, cols, ships, grid, current_ship + 1)
+    return ship_placemente_aux(rows, cols, ships, grid, current_ship + 1)
+
+
+    
+def main():
+    rows, cols, ships = parse_input('input.txt')
+    ship_placement(rows, cols, ships)
+    
+    
+
+if __name__ == '__main__':
+    main()
