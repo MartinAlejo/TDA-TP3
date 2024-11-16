8b5aabb854c4bf0ac79a32ce17c42d1a3efa5fac
RiedelNicolas <nariedel99@gmail.com>
2024-11-16 16:34:17 -0300

    working with first cases

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def print_grid(grid):
+import copy
+def print_grid(grid, demmand_rows, demmand_cols):
+
+    print("Demmand rows: ", demmand_rows)
+    print("Demmand cols: ", demmand_cols)
     print("----------------------")
     for row in grid:
         for cell in row:
@@ -6,8 +10,11 @@ def print_grid(grid):
                 print('-', end=' ')
             else:
                 print(cell, end=' ')
-        print("") 
-
+        print("")
+    print("----------------------")
+    print("")
+    print("")
+    
 def parse_input (path):
     with open(path, 'r') as file:
         lines = file.readlines()
@@ -40,56 +47,80 @@ def verify_position(grid, row, col):
                     return False
     return True
 
+def calculate_score(grid):
+    total = 0
+    for row in grid:
+        for cell in row:
+            if cell is not None:
+                total += 2
+    return total
+
 #Return a list of list, showing the placement of the ships
 def ship_placement(rows, cols, ships):
     grid = [[None] * len(rows) for _ in range(len(cols))]
-    gained_ammount = 0
+    better_solution_grid = [[None] * len(rows) for _ in range(len(cols))]
     total_amount = sum(rows) + sum(cols)
-    gained_ammount = ship_placemente_aux(rows, cols, ships, grid, 0)
-    print("Gained ammount: ", gained_ammount)
+    ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), 0, better_solution_grid)
+    print("Gained ammount: ", calculate_score(better_solution_grid))
     print("Total ammount: ", total_amount)
-    print_grid(grid)
+    print_grid(better_solution_grid, rows, cols)
 
-def ship_placemente_aux(rows, cols, ships, grid, current_ship):
+def ship_placemente_aux(rows, cols, ships, grid, current_ship, better_solution_grid):
+    
+    
     if current_ship == len(ships):
-        return 0
+        if calculate_score(grid) > calculate_score(better_solution_grid):
+            for i in range(len(grid)):
+                for j in range(len(grid[0])):
+                    better_solution_grid[i][j] = grid[i][j]
+
+    if current_ship == len(ships):
+        return
     
     for i in range(len (rows)):
         for j in range(len (cols)):
             if (rows[i] > 0 and cols[j] > 0) and verify_position(grid, i, j):
-                if i + ships[current_ship] <= len(rows):
+                if (cols[j] >= ships[current_ship]) and (i + ships[current_ship] <= len(rows)):
                     can_place = True
                     for k in range(ships[current_ship]):
                         if not verify_position(grid, i + k, j):
                             can_place = False
                             break
                     if can_place:
+                        ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), current_ship + 1, better_solution_grid)
                         for k in range(ships[current_ship]):
                             grid[i + k][j] = current_ship
                             rows[i + k] -= 1
-                        return 2*(ships[current_ship]) + ship_placemente_aux(rows, cols, ships, grid, current_ship + 1)
-                if j + ships[current_ship] <= len(cols):
+                            cols[j] -= 1
+                        ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), current_ship + 1, better_solution_grid)
+                        return
+
+                if (rows[i] >= ships[current_ship]) and (j + ships[current_ship] <= len(cols)):
                     can_place = True
                     for k in range(ships[current_ship]):
                         if not verify_position(grid, i, j + k):
                             can_place = False
                             break
                     if can_place:
+                        ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), current_ship + 1, better_solution_grid)
                         for k in range(ships[current_ship]):
                             grid[i][j + k] = current_ship
                             cols[j + k] -= 1
-                        return 2*(ships[current_ship]) + ship_placemente_aux(rows, cols, ships, grid, current_ship + 1)
-                    
-
-
-    return ship_placemente_aux(rows, cols, ships, grid, current_ship + 1)
+                            rows[i] -= 1
+                        ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), current_ship + 1, better_solution_grid)
+                        return
+    ship_placemente_aux(copy.deepcopy(rows), copy.deepcopy(cols), ships, copy.deepcopy(grid), current_ship + 1, better_solution_grid)
 
 
     
-def main():
-    rows, cols, ships = parse_input('input.txt')
+def run_example(file):
+    rows, cols, ships = parse_input("inputs/" + file)
     ship_placement(rows, cols, ships)
-    
+
+def main():
+    run_example('3_3_2.txt')
+    run_example('5_5_6.txt')
+    run_example('8_7_10.txt')
     
 
 if __name__ == '__main__':
