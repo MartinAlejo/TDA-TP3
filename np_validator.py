13f469a5045acc973d634c6a0bb6ae0ddcf9de80
MartinAlejo <mpolese@fi.uba.ar>
2024-11-28 00:33:54 -0300

    Agrego validaciones de demandas en filas y columnas (se deben cumplir exactamente). Agrego mas 'tests'

--- np_validator.py
+++ np_validator.py
@@ -24,13 +24,83 @@ def validator(solution, row_demands, col_demands, ships):
                 valid = validate_adjacency(solution, i, j, ship)
                 if valid == False:
                     return False
+
+    # We validate that the demands are being fulfilled
+    for i in range(len(row_demands)):
+        for j in range(len (col_demands)):
+            if solution[i][j] != None:
+                row_demands[i] -= 1
+                col_demands[j] -= 1
+
+    for i in range(len(row_demands)):
+        if row_demands[i] != 0:
+            return False
+
+    for j in range(len (col_demands)):
+        if col_demands[j] != 0:
+            return False
+        
+    #TODO: Quiza validar que no se esten metiendo todos los barcos y que el tamanio es correcto (no se si es necesario,
+    # pero no se esta haciendo ninguna validacion sobre los barcos)
     
+    # If it reaches here, then the solution is valid
     return True
 
 
 # TODO: Borrar
-def test():
+def test1():
+    valid_solution = [
+        [0, None, None],
+        [0, None, 1],
+        [0, None, 1]
+    ]
 
+    rows = [1, 2, 2]
+    cols = [3, 0, 2]
+    ships = [3, 2]
+
+    print(validator(valid_solution, rows, cols, ships)) # Deberia devolver true (no hay adyacencias y las demandas se cumplen)
+
+def test2():
+    solution = [
+        [0, None, None],
+        [0, 1, None],
+        [0, 1, None]
+    ] # Hay adyacencias
+
+    rows = [1, 2, 2]
+    cols = [3, 2, 0]
+    ships = [3, 2]
+
+    print(validator(solution, rows, cols, ships)) # Deberia devolver false porque hay adyacencias
+
+def test3():
+    solution = [
+        [0, None, None],
+        [0, None, 1],
+        [0, None, 1]
+    ]
+
+    rows = [1, 2, 2]
+    cols = [2, 0, 2] # No se cumple la demanda de la primer columna
+    ships = [3, 2]
+
+    print(validator(solution, rows, cols, ships)) # Deberia devolver false porque no se cumplen las demandas de columna
+
+def test4():
+    solution = [
+        [0, None, None],
+        [0, None, 1],
+        [0, None, 1]
+    ]
+
+    rows = [1, 2, 3] # No se cumple la demanda de la ultima fila
+    cols = [3, 0, 2]
+    ships = [3, 2]
+
+    print(validator(solution, rows, cols, ships)) # Deberia devolver false porque no se cumplen las demandas de fila
+
+def test5():
     solution_5_5_6 = [
         [2, None, None, 3, None],
         [2, None, None, 3, None],
@@ -42,6 +112,12 @@ def test():
     
     rows, cols, ships = parse_input("inputs/5_5_6.txt")
 
-    print(validator(solution_5_5_6, rows, cols, ships)) # Deberia devolver false al validar que las demandas se cumplan exactamente
+    print(validator(solution_5_5_6, rows, cols, ships)) # Deberia devolver false porque las demandas no se cumplen exactamente
 
-test()
+
+
+test1()
+test2()
+test3()
+test4()
+test5()
