e4700952ee1f1877f98186a309331243969f53cf
MartinAlejo <mpolese@fi.uba.ar>
2024-12-12 15:06:03 -0300

    Actualizo el informe

--- README.md
+++ README.md
@@ -17,13 +17,3 @@ El informe del trabajo se encuentra [acá](./Informe.pdf).
 Para correr el programa, se debe ejecutar el siguiente comando sobre el directorio raíz:
 
 ```python3 main.py```
-
-## Notebook para verificar complejidad algorítmica
-* Navegar al directorio `/VerificacionComplejidad`
-* Instalar el paquete python3-venv (si no está instalado): `sudo apt install python3-venv`
-* Crear el entorno virtual: `python3 -m venv myenv`
-* Iniciar el venv: `source myenv/bin/activate`
-* Instalar dependencias necesarias con: `pip install -r requirements.txt`
-* Lanzar Jupyter notebook con: `jupyter notebook`
-
-Al finalizar, para desactivar el entorno virtual: `deactivate`
--- dev_informe/code/backtracking.py
+++ dev_informe/code/backtracking.py
@@ -1,21 +1,19 @@
+    
+# Verifica si podemos colocar un barco en un casillero
 def verify_position(grid, row, col, rows, cols, demand):
 
-   
     if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]):
         return False
 
-   
     if rows[row] == 0 or cols[col] == 0:
         return False
 
     if demand == 0: 
         return
 
-  
     if grid[row][col] != None:
         return False
     
-
     for i in range(-1, 2):
         for j in range(-1, 2):
             if row + i >= 0 and row + i < len(grid) and col + j >= 0 and col + j < len(grid[0]):
@@ -24,6 +22,7 @@ def verify_position(grid, row, col, rows, cols, demand):
                 
     return True
 
+# Calcula la demanda cumplida para la grilla
 def calculate_score(grid):
     total = 0
     for row in grid:
@@ -32,6 +31,7 @@ def calculate_score(grid):
                 total += 2
     return total
 
+# Calcula la demanda que se cumple si se colocasen todos los barcos a partir de un indice
 def calculate_possible_max_ships(ships, current_index):
     score = 0
     available_ships = ships[current_index:]
@@ -42,7 +42,8 @@ def calculate_possible_max_ships(ships, current_index):
 
     return score
 
-
+# Funcion principal. Dada una demanda de filas, columnas y barcos, maximiza la demanda cumplida (minimiza la
+# demanda incumplida).
 def ship_placement(rows, cols, ships):
     grid = [[None] * len(cols) for _ in range(len(rows))]
     best_solution_grid = [[None] * len(cols) for _ in range(len(rows))]
@@ -55,30 +56,35 @@ def ship_placement(rows, cols, ships):
     print("Total ammount: ", total_amount)
     print_grid(best_solution_grid, rows, cols)
 
+# Coloca el barco horizontalmente
 def place_ship_horizontally(grid, row, col, ship_size, rows, cols):
     for k in range(ship_size):
         grid[row][col + k] = 1
         rows[row] -= 1
         cols[col + k] -= 1
 
+# Coloca el barco verticalmente
 def place_ship_vertically(grid, row, col, ship_size, rows, cols):
     for k in range(ship_size):
         grid[row + k][col] = 1
         rows[row + k] -= 1
         cols[col] -= 1
 
+# Descoloca el barco horizontalmente
 def unplace_ship_horizontally(grid, row, col, ship_size, rows, cols):
     for k in range(ship_size):
         grid[row][col + k] = None
         rows[row] += 1
         cols[col + k] += 1
-        
+
+# Descoloca el barco verticalmente
 def unplace_ship_vertically(grid, row, col, ship_size, rows, cols):
     for k in range(ship_size):
         grid[row + k][col] = None
         rows[row + k] += 1
         cols[col] += 1
 
+# Calcula la cantidad de espacios libres en la grilla
 def available_places(grid):
     empty_places = 0
 
@@ -89,36 +95,35 @@ def available_places(grid):
     
     return empty_places
 
-
+# Funcion de BT para maximizar la demanda cumplida
 def ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_ship):
-    score_grid = calculate_score(grid) # Puntaje actual
-    score_best = calculate_score(best_solution_grid) # Puntaje maximo alcanzado
+    score_grid = calculate_score(grid) 
+    score_best = calculate_score(best_solution_grid)
 
-   
+    # Si encontramos una mejor solucion, la pisamos
     if score_grid > score_best:
         for i in range(len(rows)):
             for j in range(len(cols)):
                 best_solution_grid[i][j] = grid[i][j]
     
-   
+    # Caso base
     if (current_idx_ship >= len(ships)): 
         return
 
-    available_spaces = available_places(grid)
-
-    
+    # Poda 1 (la puntuacion que podria llegar a conseguir en el mejor de los casos no supera la mejor conseguida)
     if score_grid <= score_best:
-      
+        available_spaces = available_places(grid)
         max_possible_score_ships = calculate_possible_max_ships(ships, current_idx_ship)
-        max_possible_score = score_grid + min(max_possible_score_ships, available_spaces)
-         
+        max_possible_score = score_grid + min(max_possible_score_ships, available_spaces * 2)
         if max_possible_score <= score_best:
-            return 
-        
+            return
+
+    # Poda 2 (el barco no entra para ninguna fila/columna por falta de demanda)
     maximo = max(max(rows), max(cols))
     while (ships[current_idx_ship] > maximo) and (current_idx_ship < len(ships)):
-        current_idx_ship += 1 
+        current_idx_ship += 1
     
+    # Con el barco actual, iteramos por la grilla intentando colocarlo
     ship_size = ships[current_idx_ship]
     for i in range(len(rows)):
         if rows[i] == 0: 
@@ -129,7 +134,8 @@ def ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_
             can_place_horizontal = True
             can_place_vertical = True
             demand = 0
-        
+
+            # Vemos si se puede colocar horizontal
             for k in range(ship_size):
                 if (k == 0):
                     demand = rows[i]
@@ -138,6 +144,7 @@ def ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_
                     break
                 demand -= 1
 
+            # Vemos si se puede colocar vertical
             for k in range(ship_size):
                 if (k == 0):
                     demand = cols[j]
@@ -145,6 +152,8 @@ def ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_
                     can_place_vertical = False
                     break
                 demand -= 1
+
+            # Colocamos los barcos segun corresponda
 
             if (can_place_horizontal):
                 place_ship_horizontally(grid, i, j, ship_size, rows, cols)
@@ -156,4 +165,5 @@ def ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_
                 ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_ship + 1)
                 unplace_ship_vertically(grid, i, j, ship_size, rows, cols)
 
+    # Caso en el que decidimos no colocar el barco
     ship_placement_aux(rows, cols, ships, grid, best_solution_grid, current_idx_ship + 1)
